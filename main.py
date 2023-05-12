fbf66743f7a8efcddc60720b10cc74460b18976c
FQY <qingyun-feng@qq.com>
2023-05-12 10:57:48 -0400

    structuring

--- main.py
+++ main.py
@@ -1,62 +1,22 @@
 import os
+from utils import gen_key, init_folder, generate_zip, delete_files
 from flask import Flask, redirect, request, render_template, send_file, url_for, flash
-import subprocess
-import zipfile
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
-app.config['UPLOAD_FOLDER'] = 'scratch_files'
-app.config['DOWNLOAD_FOLDER'] = 'generated_files'
-
-
-def create_zip_folder(folder_path, zip_path):
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zip_file.write(file_path, os.path.relpath(
-                    file_path, folder_path))
-
-
-def gen_key():
-    import random
-    import string
-    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
-
-
-def run_command(command):
-    process = subprocess.Popen(
-        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return_code = process.returncode
-    return stdout, stderr, return_code
-
-
-def generate_zip(key):
-    _, _, code = run_command(
-        f"cd {app.config['DOWNLOAD_FOLDER']} && mkdir {key}")
-    if code != 0:
-        return False
-    _, _, code = run_command(
-        f"python -m pystage.convert.sb3 {app.config['UPLOAD_FOLDER']}/{key}.sb3 -l en -d {app.config['DOWNLOAD_FOLDER']}/{key}")
-    if code != 0:
-        return False
-
-    create_zip_folder(f"{app.config['DOWNLOAD_FOLDER']}/{key}",
-                      f"{app.config['DOWNLOAD_FOLDER']}/{key}.zip")
-    return True
+app.config['UF'] = 'scratch_files'
+app.config['DF'] = 'generated_files'
 
 
 @app.route('/')
 def index():
     key = request.args.get('key')
-    path = f"{app.config['DOWNLOAD_FOLDER']}/{key}/{key}.py"
+    path = f"{app.config['DF']}/{key}/{key}.py"
     if key and os.path.exists(path):
         with open(path, 'r') as f:
             code = f.read()
-            print(code)
         return render_template('index.html', key=key, code=code, ok=1)
-    return render_template('index.html', key=key)
+    return render_template('index.html')
 
 
 @app.route('/upload', methods=['POST'])
@@ -66,18 +26,25 @@ def upload_file():
         flash('No file selected!')
         return redirect(url_for('index'))
     key = gen_key()
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{key}.sb3"))
+    file.save(os.path.join(app.config['UF'], f"{key}.sb3"))
 
     # flash('Generating file...')
-    rst = generate_zip(key)
-    if not rst:
+    rst = generate_zip(app.config, key)
+    if not rst[0]:
         flash('Error in generating file!')
+        flash(rst[1])
+        flash(rst[2])
+    flash('File generated successfully!')
+    # for line in rst[1].split('\r\n'):
+    #     flash(line)
     return redirect(url_for('index', key=key))
 
 
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_file(os.path.join(app.config['DOWNLOAD_FOLDER'], f"{filename}.zip"), as_attachment=True)
+    return send_file(os.path.join(app.config['DF'], f"{filename}.zip"), as_attachment=True)
 
 
-app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    init_folder(app.config)
+    app.run(debug=True, host='0.0.0.0')
---
+++ utils.py
@@ -0,0 +1,75 @@
+import os
+import subprocess
+import time
+import zipfile
+
+
+def init_folder(config: dict):
+    if not os.path.exists(config['UF']):
+        os.mkdir(config['UF'])
+    if not os.path.exists(config['DF']):
+        os.mkdir(config['DF'])
+
+
+def gen_key():
+    """return time stamp"""
+    return str(int(time.time()))
+
+
+def run_command(command):
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    stdout = stdout.decode('utf-8')
+    stderr = stderr.decode('utf-8')
+    return_code = process.returncode
+    return stdout, stderr, return_code
+
+
+def create_zip_folder(folder_path, zip_path):
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zip_file.write(file_path, os.path.relpath(
+                    file_path, folder_path))
+
+
+def generate_zip(config, fn):
+    _, _, code = run_command(
+        f"cd {config['DF']} && mkdir {fn}")
+    if code != 0:
+        return False, None, None
+    out, err, code = run_command(
+        f"python -m pystage.convert.sb3 {config['UF']}/{fn}.sb3 -l en -d {config['DF']}/{fn}")
+    if code != 0:
+        return False, out, err
+
+    create_zip_folder(f"{config['DF']}/{fn}",
+                      f"{config['DF']}/{fn}.zip")
+    return True, out, err
+
+
+def delete_folder(path):
+    """delete a folder by recursion"""
+    for file in os.listdir(path):
+        """if it is a direcotry, delete all files in it"""
+        if os.path.isdir(os.path.join(path, file)):
+            delete_folder(os.path.join(path, file))
+        else:
+            os.remove(os.path.join(path, file))
+    os.rmdir(path)
+    return True
+
+
+def delete_files(config):
+    """delete all files in UF and DF"""
+    for file in os.listdir(config['DF']):
+        """if it is a direcotry, delete all files in it"""
+        if os.path.isdir(os.path.join(config['DF'], file)):
+            delete_folder(os.path.join(config['DF'], file))
+        else:
+            os.remove(os.path.join(config['DF'], file))
+    for file in os.listdir(config['UF']):
+        os.remove(os.path.join(config['UF'], file))
+    return True
