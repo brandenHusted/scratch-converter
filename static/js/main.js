3d0cacb9a670994fb518904e3bc0ca90d8225187
brandenHusted <blh94@calvin.edu>
2023-05-31 14:20:02 -0400

    fix the tutorial

--- static/js/main.js
+++ static/js/main.js
@@ -84,6 +84,7 @@ const step2_2 = $(".step2.file");
 const step3 = $(".step3");
 const step4 = $(".step4");
 const tutorial = $(".tutorial_dog")
+const tutorialnext1 = $(".tutorialNext1")
 let jumpThrough = "";
 
 $("#languageOptions").val($("#languageOptions").attr("data-lang"));
@@ -114,10 +115,30 @@ $("#tutorial1").on("click", () => {
   tutorial.show();
   
 })
-
+// pages on tutorial for back and next
 const backToStep1FromTutorial = $(".tutorial_dog .back");
 backToStep1FromTutorial.on("click", () => {
   tutorial.hide();
+  step1.show();
+});
+
+const toPageTwoTutorial = $(".tutorial_dog .next");
+toPageTwoTutorial.on("click", () => {
+  tutorial.hide();
+  tutorialnext1.show();
+  
+});
+
+const backToPageOneTutorial = $(".tutorialNext1 .back");
+backToPageOneTutorial.on("click", () => {
+  tutorialnext1.hide();
+  tutorial.show();
+  
+});
+
+const backToStartFromTutorial = $(".tutorialNext1 .next");
+backToStartFromTutorial.on("click", () => {
+  tutorialnext1.hide();
   step1.show();
 });
 
